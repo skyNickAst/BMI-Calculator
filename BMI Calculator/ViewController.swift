c71414a3e9d54fb843163c3366098725ce753731
Nikolai Astakhov <skynetprojectone@gmail.com>
2022-12-22 23:43:51 +0300

    Prepare main functionality, now app can print BMI.

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var hightValueLabel: UILabel!
+    @IBOutlet weak var weightValueLabel: UILabel!
+    @IBOutlet weak var hightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var hightValue = 170
+    var weightValue = 60
+    
+    
+    @IBAction func hightSliderChanged(_ sender: UISlider) {
+        hightValue = Int(sender.value)
+        hightValueLabel.text = String("\(hightValue) sm")
     }
-
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightValue = Int(sender.value)
+        weightValueLabel.text = String("\(weightValue) kg")
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        let a = calculateBMI()
+        print(a)
+        
+    }
+    
+    func calculateBMI() -> String {
+        let hightDevided = Float(hightValue) / 100
+        let result = Float(weightValue) / (hightDevided * hightDevided)
+        let bmi = String(format: "%.1f", result)
+        return bmi
+    }
 }
 
